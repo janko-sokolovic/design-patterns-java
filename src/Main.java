1325540a79b26a8ea2c5f0abb2a0cf77ed67614b
Janko <jasofalcon@gmail.com>
2017-11-13 18:47:43 +0100

    Add Builder, fix formatting

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import builder.House;
+import builder.House.HouseBuilder;
 import factory.Car;
 import factory.CarFactory;
 import observer.AppNotificationObserver;
@@ -8,6 +10,8 @@ import strategy.SendEmailMessageStrategy;
 import strategy.SendLetterMessageStrategy;
 import strategy.SendMessageStrategy;
 
+import static java.lang.System.out;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -17,18 +21,23 @@ public class Main {
         testStrategy();
 
         testObserver();
+
+        testBuilder();
     }
 
+
     private static void testFactory() {
+        out.println("---- Testing Factory Pattern ----");
         Car electricCar = CarFactory.create("electric");
         Car iceCar = CarFactory.create("ice");
 
-        System.out.println("Electric car: " + electricCar);
-        System.out.println("Electric car: " + iceCar);
-
+        out.println("Electric car: " + electricCar);
+        out.println("Electric car: " + iceCar);
+        out.println();
     }
 
     private static void testStrategy() {
+        out.println("---- Testing Strategy Pattern ----");
         SendMessageStrategy emailStrategy = new SendEmailMessageStrategy();
         SendMessageStrategy letterStrategy = new SendLetterMessageStrategy();
 
@@ -40,9 +49,11 @@ public class Main {
         context.setStrategy(letterStrategy);
         context.sendMessage();
 
+        out.println();
     }
 
     private static void testObserver() {
+        out.println("---- Testing Observer Pattern ----");
         Blog blog = new Blog();
 
         new EmailObserver(blog);
@@ -50,6 +61,19 @@ public class Main {
 
         blog.addPost("New interesting topic");
 
+        out.println();
+    }
+
+
+    private static void testBuilder() {
+        out.println("---- Testing Builder Pattern ----");
+        HouseBuilder houseBuilder = new HouseBuilder("red-roof", "glass-windows");
+        House house = houseBuilder.setIsSmartHouse(true)
+                //.hasPorch(false)   // We do not need to provide optionals.
+                .setGarage("smart-garage")
+                .build();
+
+        out.println("House created -> " + house);
     }
 
 }
---
+++ src/builder/House.java
@@ -0,0 +1,62 @@
+package builder;
+
+public class House {
+
+    // required
+    private String roof;
+    private String windows;
+
+    // optional
+    private String garage;
+    private boolean isSmartHouse;
+    private boolean hasPorch;
+
+    private House(HouseBuilder houseBuilder) {
+        this.roof = houseBuilder.roof;
+        this.windows = houseBuilder.windows;
+        this.garage = houseBuilder.garage;
+        this.isSmartHouse = houseBuilder.isSmartHouse;
+        this.hasPorch = houseBuilder.hasPorch;
+    }
+
+    @Override
+    public String toString() {
+        return "roof: " + roof + ", windows: " + windows + ", garage: " + garage + ", smart?: " + isSmartHouse + ", has porch?: " + hasPorch;
+    }
+
+    public static class HouseBuilder {
+        // required
+        private String roof;
+        private String windows;
+
+        // optional
+        private String garage;
+        private boolean isSmartHouse;
+        private boolean hasPorch;
+
+        public HouseBuilder(String roof, String windows) {
+            this.roof = roof;
+            this.windows = windows;
+        }
+
+        public HouseBuilder setGarage(String garage) {
+            this.garage = garage;
+            return this;
+        }
+
+        public HouseBuilder setIsSmartHouse(boolean isSmartHouse) {
+            this.isSmartHouse = isSmartHouse;
+            return this;
+        }
+
+        public HouseBuilder hasPorch(boolean hasPorch) {
+            this.hasPorch = hasPorch;
+            return this;
+        }
+
+        public House build() {
+            return new House(this);
+        }
+
+    }
+}
